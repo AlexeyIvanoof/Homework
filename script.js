63f62371da4224b2734344a439f44b60cc37828c
Alexei Ivanov <alekseyivanoof34@gmail.com>
2023-05-15 22:23:04 +0700

    практика в Js

--- script.js
+++ script.js
@@ -1,57 +1,73 @@
-let a;
-a=alert(10)
-a=alert(20)//Задание 1
+//Домашка 3. js
+//Задание1
+let password = 'Скайпро';
+let input=prompt("Введите пароль")
+if (input === password) {
+  console.log('Пароль введен верно');
+} else {
+  console.log('Пароль введен неправильно');
+}
+//Задание2
+let c = 5;
+if (c > 0 && c < 10) {
+  console.log("Верно");
+} else {
+  console.log("Неверно");
+}
+//Задание3
+let d = 120;
+let e = 80;
+if (d > 100 || e > 100) {
+    console.log("Верно");
+} else {
+    console.log("Неверно");  
+}
+//Задание4
+let a = '2';
+let b = '3';
+alert(parseInt(a) + parseInt(b));// синтаксический анализ.
 
-let year=2007
-alert(`год выпуска первого iPhone 9 января ${year} года` )//Задание 2
+//Задание 5
+let monthNumber= String(prompt("Введите число месяца"));
+switch (monthNumber) {
+    case "1":
+        console.log("январь-Зима")
+        break;
+        case "2":
+        console.log("февраль-Зима")
+        break;
+        case "3":
+        console.log("март-Весна")
+        break;
+        case "4":
+        console.log("апрель-Весна")
+        break;
+        case "5":
+        console.log("май-Весна")
+        break;
+        case "6":
+        console.log("июнь-Лето")
+        break;
+        case "7":
+        console.log("июль-Лето")
+        break;
+        case "8":
+        console.log("август-Лето")
+        break;
+        case "9":
+        console.log("сентябрь-Осень")
+        break;
+        case "10":
+        console.log("октябрь-Осень")
+        break;
+        case "11":
+        console.log("ноябрь-Осень")
+        break;
+        case "12":
+        console.log("декабрь-Зима")
+        break;
 
-let creator="Брендан Айк";
-alert(`создатель языка JavaScript ${creator}`);//Задание 3
-
-let b=10;
-let c=2;
-let d=Number(b) + Number(c);
-alert(d);
-let e=Number(b) - Number(c);
-alert(e);
-let f=Number(b) * Number(c);
-alert(f);
-let j=Number(b) / Number(c);
-alert(j);//Задание 4
-
-alert(c**5)//2 в 5й степени Задание 5
-
-let i=9
-alert(i%2)// остаток от деления 9/2 Задание 6
-
-let num = 1;
-num += 5;
-num -= 3;
-num *= 7;
-num /= 3;
-num ++;
-num --;
-alert(num);// использование операторов присваивания Задание 7
-
-let age =(prompt ("Сколько вам лет?"));
-alert(age)//Задание 8
-
-const user={
-    name: "Ivan",
-    age:35,
-    isAdmin:true
-}// Задание 9
-
-user['city of residence']=true;// Задание 9.1
-
-user.age=37;
-console.log(user)//Задание 9.2
-
-delete user['city of residence']// Задание 9.3
-
-let info=prompt("Какую информацию хотите узнать о пользователе?")
-console.log(user[info]);
-alert(user[info])//Задание 9.4
-
-let name=prompt("Назовите свое имя");
-alert(`Привет, ${name}!`);//Задание 10
+    default:
+        console.log("Неверно")
+        break;
+}
