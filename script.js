284179c22e78ffa247d6619419dc86475e212a77
Alexei Ivanov <alekseyivanoof34@gmail.com>
2023-05-18 22:31:25 +0700

    дз 4 работа с джава.

--- script.js
+++ script.js
@@ -1,73 +1,46 @@
-//Домашка 3. js
-//Задание1
-let password = 'Скайпро';
-let input=prompt("Введите пароль")
-if (input === password) {
-  console.log('Пароль введен верно');
-} else {
-  console.log('Пароль введен неправильно');
-}
-//Задание2
-let c = 5;
-if (c > 0 && c < 10) {
-  console.log("Верно");
-} else {
-  console.log("Неверно");
-}
-//Задание3
-let d = 120;
-let e = 80;
-if (d > 100 || e > 100) {
-    console.log("Верно");
-} else {
-    console.log("Неверно");  
-}
-//Задание4
-let a = '2';
-let b = '3';
-alert(parseInt(a) + parseInt(b));// синтаксический анализ.
+// домашка 4
 
+//Задание 1
+let i = 1;
+while (i<=2) {
+  console.log("Привет");
+  i++;
+}
+//Задание 2
+let a = 1;
+while (a < 6) {
+  console.log(a);
+  a++;
+}
+//Задание 3
+let b = 7;
+while (b <= 22) {
+  console.log(b);
+  b++;
+}
+//Задание 4
+const obj={
+  'Коля':20000,
+  'Вася':30000,
+  'Петя':40000
+};
+for (let salary in obj) {
+  console.log(`${salary} зарплата ${obj[salary]} рублей`);
+}
 //Задание 5
-let monthNumber= String(prompt("Введите число месяца"));
-switch (monthNumber) {
-    case "1":
-        console.log("январь-Зима")
-        break;
-        case "2":
-        console.log("февраль-Зима")
-        break;
-        case "3":
-        console.log("март-Весна")
-        break;
-        case "4":
-        console.log("апрель-Весна")
-        break;
-        case "5":
-        console.log("май-Весна")
-        break;
-        case "6":
-        console.log("июнь-Лето")
-        break;
-        case "7":
-        console.log("июль-Лето")
-        break;
-        case "8":
-        console.log("август-Лето")
-        break;
-        case "9":
-        console.log("сентябрь-Осень")
-        break;
-        case "10":
-        console.log("октябрь-Осень")
-        break;
-        case "11":
-        console.log("ноябрь-Осень")
-        break;
-        case "12":
-        console.log("декабрь-Зима")
-        break;
-        
-    default:
-        console.log("Неверно")
-        break;
+let n=1000;
+let nam=0;
+
+while (n >= 50) {
+ n/=2;
+ nam +=1;
+ console.log(nam, n)
+}
+//Задание 6
+let fr=0;
+let wk=7;
+while(wk < 31){
+  fr=wk-2;
+  wk+=7;
+  console.log(`Сегодня пятница, ${fr}-е число. Необходимо подготовить отчет.`);
 }
