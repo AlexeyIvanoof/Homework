501b00d03c3d770d12fc2b3d4a08a1db632ae113
Alexei Ivanov <alekseyivanoof34@gmail.com>
2023-05-24 17:49:59 +0700

    домашка 5

--- script.js
+++ script.js
@@ -1,47 +1,120 @@
-// домашка 4
+//Домашка 5
 
 //Задание 1
-let i = 1;
-while (i<=2) {
-  console.log("Привет");
-  i++;
+
+function number(a, b) {
+  if (a < b) {
+    return a;
+  } else {
+    return b;
+  }
+    };
+  
+  console.log(number(4, 8));
+  
+  //Задание 2
+  
+  function numb(c) {
+    if (c % 2 == 0) {
+      return('Число четное');
+    } else {
+      return('Число нечетное');
+    }
+  };
+  console.log(numb(3))
+  
+  //Задание 3
+  
+  function numbr(d) {
+    return d**=2;
+  }
+  console.log(numbr(6));
+  
+  //Задание 4
+  let answer = Number(prompt('Сколько вам лет?'));
+  function question(answer) {
+    if ( answer <= 12 && answer >= 0) {
+      return 'Привет, друг!';
+    } else if (answer >= 13 && answer >= 0){
+      return'Добро пожаловать!';
+    } else {
+      return 'Не правильное значение';
+  }
+  }
+  alert(question(answer));
+  
+  //Задание 5
+  let x = Number(prompt('Задание 5: Введите первое число'));
+  let y = Number(prompt('Введите второе число'));
+  function num(x, y) {
+    if (isNaN(x) && isNaN(y)) {
+      return 'Одно или оба значения не являются числом';
+    } else {
+      return x*y;
+    }
+  }
+  alert(num(x, y));
+  
+  //Задание 6
+  
+  let z = Number(prompt('Задание 6: Введите число'));
+  function examin(z) {
+    if (isNaN(z)) {
+      return 'Переданный параметр не является числом';
+    } else {
+      return z**=2;
+    }
+  }
+  alert(examin(z));
+  
+  //Задание 7
+  
+  const circle1 = {
+    radius :20,
+    getArea:function Area() {
+      let r = 20;
+      let p = 3.14;
+        return p * (r **= 2)
+    },
+    getPerimeter:function Perimeter () {
+      let r = 20;
+      let p = 3.14;
+        return 2 * p * r
+  },
+  };
+  console.log(`Площадь равна ${circle1.getArea()} ,Периметр равен ${circle1.getPerimeter()}`);
+  
+  const circle2 = {
+    radius :15,
+    getArea:function Area() {
+      let r = 15;
+      let p = 3.14;
+        return p * (r **= 2)
+    },
+    getPerimeter:function Perimeter () {
+      let r = 15;
+      let p = 3.14;
+        return 2 * p * r
+  },
+  };
+  console.log(`Площадь равна ${circle2.getArea()} ,Периметр равен ${circle2.getPerimeter()}`);
+  // думаю что не совсем верно исполнил))
+
+//Задание 8
+
+function monthNumber() {
+  let month = String(prompt("Введите число месяца"));
+
+  if (month >= 3 && month <= 5) {
+    alert ('Весна')
+  } else if (month >= 6 && month <= 8) {
+    alert ('Лето')
+  }else if (month >= 9 && month <= 11) {
+    alert ('Осень')
+  }else if (month == 1 || month ==2 || month ==12)  {
+    alert ('Зима')
+  }else if (isNaN(month) || month <= 0)  {
+    alert ('Неправильный параметр!')
 }
-//Задание 2
-let a = 1;
-while (a < 6) {
-  console.log(a);
-  a++;
-}
-//Задание 3
-let b = 7;
-while (b <= 22) {
-  console.log(b);
-  b++;
-}
-//Задание 4
-const obj={
-  'Коля':20000,
-  'Вася':30000,
-  'Петя':40000
 };
-for (let salary in obj) {
-  console.log(`${salary} зарплата ${obj[salary]} рублей`);
-}
-
-//Задание 5
-let n=1000;
-let nam=0;
-
-while (n >= 50) {
- n/=2;
- nam +=1;
- console.log(nam, n)
-}
-//Задание 6
-let fr=0;
-let wk=7;
-while(wk < 31){
-  fr=wk-2;
-  wk+=7;
-  console.log(`Сегодня пятница, ${fr}-е число. Необходимо подготовить отчет.`);
-}
+console.log(monthNumber());
